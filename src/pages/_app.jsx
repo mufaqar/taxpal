33614fc7c63b97ce4da146b948a318db58432f4f
Mufaqar Islam <mufaqar@gmail.com>
2022-08-01 15:00:54 +0500

    apollo and gqo

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,14 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../../lib/apollo'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  )
 }
